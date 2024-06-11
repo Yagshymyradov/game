9152bf26969217056d86cdd58722ce8f09980ab5
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2024-06-11 11:40:25 +0500

    setup

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+const double gameWidth = 1080;
+const double gameHeight = 1920;
---
+++ lib/game_app.dart
@@ -0,0 +1,40 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:game/constants.dart';
+
+import 'go_green.dart';
+
+class GameApp extends StatefulWidget {
+  const GameApp({super.key});
+
+  @override
+  State<GameApp> createState() => _GameAppState();
+}
+
+class _GameAppState extends State<GameApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.yellow
+      ),
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: FittedBox(
+              child: SizedBox(
+                width: gameWidth,
+                height: gameHeight,
+                child: GameWidget(
+                  game: GoGreen(),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
